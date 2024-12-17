733ea5448ebed8652bf5310d7c34dfcda165ccc3
Kat <1112224017@gap.wzu.edu.tw>
2024-12-17 16:01:47 +0800

    Update views.py

--- adbotapp/views.py
+++ adbotapp/views.py
@@ -7,6 +7,8 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
 
+from datetime import datetime
+
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
@@ -27,12 +29,19 @@ def callback(request):
             return HttpResponseBadRequest()
 
         for event in events:
+
             # 若有訊息事件
             if isinstance(event, MessageEvent):
+
+                currentDateAndTime = datetime.now()
+                currentTime = currentDateAndTime.strftime("%H:%M:%S")
+                txtmsg = "您所傳的訊息是:\n"
+                txtmsg += event.message.text
+
                 # 回傳收到的文字訊息
                 line_bot_api.reply_message(
                     event.reply_token,
-                    TextSendMessage(text=event.message.text))
+                    TextSendMessage(text= txtmsg ))
 
         return HttpResponse()
     else:
