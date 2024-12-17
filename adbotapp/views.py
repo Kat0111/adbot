3f225dc85e66104dd0588f4b8e55219e85b16e0e
Kat <1112224017@gap.wzu.edu.tw>
2024-12-17 17:35:16 +0800

    Update views.py

--- adbotapp/views.py
+++ adbotapp/views.py
@@ -5,10 +5,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage
+from linebot.models import MessageEvent, TextSendMessage, StickerSendMessage, ImageSendMessage
 
 from datetime import datetime
-from linebot.models import ImageSendMessage
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
@@ -45,8 +44,12 @@ def callback(request):
                 line_bot_api.reply_message(
                     event.reply_token,
                     [TextSendMessage(text = txtmsg ),
+
                     StickerSendMessage(package_id=1070, sticker_id=17842),
-                    StickerSendMessage(package_id=1070, sticker_id=17843),
+                    
+                    ImageSendMessage(original_content_url='https://d3gjxtgqyywct8.cloudfront.net/o2o/image/961f8641-dcbe-4205-b75c-0246a292b6c4.jpg'),
+
+                    LocationSendMessage(title='文澡外語大學', address='Wenzao Ursuline University of Languages', latitude=22.670419222110695, longitude=120.31825742491581)
                     ])
 
         return HttpResponse()
